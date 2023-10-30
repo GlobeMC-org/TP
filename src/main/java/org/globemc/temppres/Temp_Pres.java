cc1f12138f840c17ca3f32cb68bf8cf693538017
leviiloviee <107021730+LeviLovie@users.noreply.github.com>
2023-10-30 23:20:50 +0700

    Redo to use in-game effects

--- src/main/java/org/globemc/temppres/Temp_Pres.java
+++ src/main/java/org/globemc/temppres/Temp_Pres.java
@@ -7,36 +7,42 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
 import java.util.logging.Logger;
 
 public final class Temp_Pres extends JavaPlugin implements Listener {
     public Logger l = getLogger();
     public Server s = getServer();
-    private int counter = 1;
+//    private int counter = 1;
 
     private double minDamageHeight = 0d;
-    private double damage = 1;
-    private int delay = 5;
+//    private double damage = 1;
+//    private int delay = 5;
 
     @Override
     public void onEnable() {
         l.info("Conditions plugin started");
         s.getPluginManager().registerEvents(this, this);
-        Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
-            @Override
-            public void run() {
-                counter += 1;
-                if (counter > delay) {
-                    counter = 1;
-                }
-            }
-        }, 0, 1L);
+//        Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+//            @Override
+//            public void run() {
+//                counter += 1;
+//                if (counter > delay) {
+//                    counter = 1;
+//                }
+//            }
+//        }, 0, 1L);
     }
 
     @EventHandler
     public void OnPlayerMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
-        if (p.getLocation().y() <= minDamageHeight)
-            if (counter == 1) p.damage(damage);
+        if (p.getLocation().y() < minDamageHeight) {
+            p.addPotionEffect(new PotionEffect(PotionEffectType.DARKNESS, 100, 2));
+            p.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 100, 2));
+        }
+//            if (counter == 1) p.damage(damage);
     }
 }
